6e4e1f5c0c322d861b07c539e02dfd729c0a6dc9
Ilya Karachevtsev <z1o7@bk.ru>
2026-03-14 09:51:54 +0300

    New print_author

--- main.py
+++ main.py
@@ -1,2 +1,11 @@
 print('Hello from main!')
 print(55)
+
+from dotenv import load_dotenv 
+import os  
+
+def print_author():
+    load_dotenv(dotenv_path='.env')
+    author = os.getenv('AUTHOR')
+    print(f"Автор проекта: {author}") 
+print_author()
